ab2f4c80587c1dc5d907291866ff4afb7f7c23c0
Nathan <me@natho.io>
2020-07-12 18:54:46 +0930

    Write base structure

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,10 +1,24 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "pch.h"
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
+#include <Windows.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <SubAuth.h>
+#include <string>
+
+#include <openssl/sha.h>
+
+#pragma comment(lib, "Ws2_32.lib")
+#pragma comment(lib, "Advapi32.lib")
+
+/* Visual Studio Boilerplate */
+
+BOOL APIENTRY DllMain(
+        HMODULE hModule,
+        DWORD  ul_reason_for_call,
+        LPVOID lpReserved
+    )
 {
     switch (ul_reason_for_call)
     {
@@ -17,3 +31,52 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     return TRUE;
 }
 
+/* Get API URI from Registry */
+LONG GetAPILocation(std::wstring& strValue)
+{
+    HKEY hKey;
+    LONG lRes = RegOpenKeyExW(HKEY_LOCAL_MACHINE, L"SOFTWARE\\AD-CompromisedPasswords", 0, KEY_READ, &hKey);
+
+    if (ERROR_SUCCESS != lRes)
+    {
+        return lRes;
+    }
+
+    WCHAR szBuffer[512];
+    DWORD dwBufferSize = sizeof(szBuffer);
+    ULONG nError;
+
+    nError = RegQueryValueExW(hKey, L"URI", 0, NULL, (LPBYTE)szBuffer, &dwBufferSize);
+
+    if (ERROR_SUCCESS == nError)
+    {
+        strValue = szBuffer;
+    }
+
+    return nError;
+}
+
+/* Generate SHA1 Hash from Input String */
+CHAR GenerateSHA1()
+{
+    // TODO
+}
+
+extern "C" __declspec(dllexport) BOOLEAN __stdcall PasswordFilter(PUNICODE_STRING AccountName, PUNICODE_STRING FullName, PUNICODE_STRING Password, BOOLEAN SetOperation)
+{
+    /* Declare Result as TRUE by Default */
+    BOOLEAN Result = TRUE;
+
+    /* Get API URI */
+    std::wstring strAPILocation;
+    GetAPILocation(strAPILocation);
+
+    /* Generate Hash and K Anonymity */
+
+
+    /* Clear Password from Memory */
+    SecureZeroMemory(Password, sizeof(Password));
+
+    /* Return Result */
+    return Result;
+}
